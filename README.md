b889f7c43e579f1657fbf9818fd6f799677e67e7
LakshaJael <nihongotraveler@icloud.com>
2017-07-21 13:22:37 +0900

    First commit, may add safties later. Read the readme file for more info

---
+++ README.md
@@ -0,0 +1,12 @@
+# ruby_filename_changer
+
+THIS IS NOT SAFE.
+
+Be very careful about using this, as there are no safeties attached. Right now it only reads
+.jpg images. 
+
+Source directory is hardcoded, but can easily be changed to ask the user which source
+directory to use. 
+
+This will rename and move any .jpg files in the source directory to a destination directory.
+
---
+++ filename_changer.rb
@@ -0,0 +1,38 @@
+require 'yaml'
+
+current_dir = Dir.getwd
+home_dir = Dir.home
+
+puts 'Current directory: ' + current_dir
+puts "Type in your destination folder directory path: #{home_dir}"
+folder_dir = gets.chomp
+
+
+Dir.chdir "#{home_dir}/#{folder_dir}"
+
+#This is source directory. Should hardcode this for a common SD Card path
+#that is commonly used.
+
+pic_names = Dir['/Users/nathanielnasarow/Desktop/rubytestsource/*.jpg']
+
+puts 'What do you want to call this batch?'
+batch_name = gets.chomp
+
+print "Downloading #{batch_name} files."
+pic_number = 1
+
+pic_names.each do |name|
+  print "."
+
+  new_name = if pic_number < 10
+    "#{batch_name}0#{pic_number}.jpg"
+  else
+    "#{batch_name}#{pic_number}.jpg"
+  end
+#Do a check here to see if file name already exists
+  File.rename name, new_name
+  pic_number = pic_number + 1
+end
+puts "Done, sexy!"
+
+
